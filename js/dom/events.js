c5bccb552edcfa8ee1884e0e78f0c7d00e6782d3
Olatz <olatzglez@gmail.com>
2026-04-14 10:37:30 +0200

    Eliminar archivos duplicados y obsoletos.

--- js/dom/events.js
+++
@@ -1,8 +0,0 @@
-export function initHeaderEvents() {
-  const toggle = document.getElementById("menuToggle");
-  const nav = document.getElementById("nav");
-
-  toggle.addEventListener("click", () => {
-    nav.classList.toggle("active");
-  });
-}
